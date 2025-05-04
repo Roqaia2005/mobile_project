d57fbb478554cf62d29e2deb09b9351a90bfb1cd
Marawan Shrief <147545917+Sharawelly@users.noreply.github.com>
2025-05-04 23:14:15 +0300

    Delete lib directory

--- lib/cubit/search_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'search_cubit.dart';
-
-
-abstract class SearchState {}
-
-class SearchInitial extends SearchState {}
-//result found, no result found,etc
--- lib/models/user.dart
+++
@@ -1,2 +0,0 @@
-//user model
-class User {}
